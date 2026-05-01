9704efd9fa0f6acee6017aa96e77649a90dac0ac
Jwanderer73 <jakewanderer8@gmail.com>
2026-05-01 13:43:56 -0700

    lab5 done

--- projects/projects.js
+++ projects/projects.js
@@ -12,6 +12,22 @@ if (projectsTitle) {
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let selectedYear = null;
+let query = '';
+
+function applyFilters() {
+  let queryFiltered = projects.filter((project) => {
+    let values = Object.values(project).join('\n').toLowerCase();
+    return values.includes(query.toLowerCase());
+  });
+
+  renderPieChart(queryFiltered);
+
+  const displayed = selectedYear
+    ? queryFiltered.filter((p) => p.year === selectedYear)
+    : queryFiltered;
+  renderProjects(displayed, projectsContainer, 'h2');
+}
 
 function renderPieChart(projectsGiven) {
   let newRolledData = d3.rollups(
@@ -26,58 +42,37 @@ function renderPieChart(projectsGiven) {
   let newArcData = newSliceGenerator(newData);
   let newArcs = newArcData.map((d) => arcGenerator(d));
 
-  d3.select('#projects-pie-plot').selectAll('path').remove();
-  d3.select('.legend').selectAll('li').remove();
+  let svg = d3.select('#projects-pie-plot');
+  svg.selectAll('path').remove();
+  let legend = d3.select('.legend');
+  legend.selectAll('li').remove();
 
   newArcs.forEach((arc, idx) => {
-    d3.select('#projects-pie-plot')
+    const isSelected = newData[idx].label === selectedYear;
+    svg
       .append('path')
       .attr('d', arc)
-      .attr('fill', colors(idx));
+      .attr('fill', colors(idx))
+      .attr('class', isSelected ? 'selected' : '')
+      .on('click', () => {
+        selectedYear = selectedYear === newData[idx].label ? null : newData[idx].label;
+        applyFilters();
+      });
   });
 
-  let legend = d3.select('.legend');
   newData.forEach((d, idx) => {
     legend
       .append('li')
       .attr('style', `--color:${colors(idx)}`)
+      .attr('class', d.label === selectedYear ? 'selected' : '')
       .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
   });
 }
 
-renderPieChart(projects);
+applyFilters();
 
-let query = '';
 let searchInput = document.querySelector('.searchBar');
-
-function setQuery(newQuery) {
-  query = newQuery;
-  return projects.filter((project) => {
-    let values = Object.values(project).join('\n').toLowerCase();
-    return values.includes(query.toLowerCase());
-  });
-}
-
 searchInput.addEventListener('change', (event) => {
-  let filteredProjects = setQuery(event.target.value);
-  renderProjects(filteredProjects, projectsContainer, 'h2');
-  renderPieChart(filteredProjects);
-});
-let selectedIndex = -1;
-let svg = d3.select('svg');
-svg.selectAll('path').remove();
-arcs.forEach((arc, i) => {
-  svg
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', colors(i))
-    .on('click', () => {
-      selectedIndex = selectedIndex === i ? -1 : i;
-    
-      svg
-        .selectAll('path')
-        .attr('class', (_, idx) => (
-          // TODO: filter idx to find correct pie slice and apply CSS from above
-        ));
-    });
+  query = event.target.value;
+  applyFilters();
 });
