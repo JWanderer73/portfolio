eabbd5c541fb5f37797d231708cdb8dbedfa5bb6
Jwanderer73 <jakewanderer8@gmail.com>
2026-05-09 00:38:01 -0700

    lab6 done

--- global.js
+++ global.js
@@ -8,6 +8,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'resume/', title: 'Resume' },
     { url: 'contact/', title: 'Contact' },
+    { url: 'meta/', title: 'Meta' },
     { url: 'https://github.com/JWanderer73', title: 'GitHub' },
   ];
 const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
